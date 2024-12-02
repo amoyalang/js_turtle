80545f9ea11f3d11418320011a5ff98ca5601b59
amoyalang <3271405674@qq.com>
2024-12-02 16:11:37 +0800

    适用于实际机器人

--- js_turtle/src/js_turtle.cpp
+++ js_turtle/src/js_turtle.cpp
@@ -1,46 +1,50 @@
-#include<ros/ros.h>  
-#include<geometry_msgs/Twist.h>  
-#include <sensor_msgs/Joy.h>  
-#include<iostream>  
-using namespace std;  
-  
-class Teleop  
-{  
-public:  
-    Teleop();  
-  
-private:  
-    /* data */  
-    void callback(const sensor_msgs::Joy::ConstPtr& Joy);  
-    ros::NodeHandle n; //实例化节点  
-    ros::Subscriber sub ;  
-    ros::Publisher pub ;  
-    double vlinear,vangular;//我们控制乌龟的速度，是通过这两个变量调整  
-    int axis_ang,axis_lin;  //axes[]的键  
-};  
-  
-Teleop::Teleop()  
-{     
-//我们将这几个变量加上参数，可以在参数服务器方便修改  
- n.param<int>("axis_linear",axis_lin,1); //默认axes[1]接收速度   
-n.param<int>("axis_angular",axis_ang,2);//默认axes[2]接收角度
-n.param<double>("vel_linear",vlinear,10);//默认线速度1 m/s
-n.param<double>("vel_angular",vangular,10);//默认角速度1 单位rad/s
-pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",1);//将速度发给乌龟
-sub = n.subscribe<sensor_msgs::Joy>("joy",10,&Teleop::callback,this); //订阅游戏手柄发来的数据
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <iostream>
+using namespace std;
+
+class Teleop
+{
+public:
+    Teleop(ros::NodeHandle &input_n, double linear_vel, double angular_vel);
+
+private:
+    void callback(const sensor_msgs::Joy::ConstPtr& joy_msg);
+    ros::NodeHandle n;
+    ros::Subscriber sub;
+    ros::Publisher pub;
+    double vlinear, vangular; // 控制速度
+    int axis_ang, axis_lin;   // 游戏手柄轴映射
+};
+
+Teleop::Teleop(ros::NodeHandle &input_n, double linear_vel, double angular_vel)
+    : n(input_n), vlinear(linear_vel), vangular(angular_vel)
+{
+    n.param<int>("axis_linear", axis_lin, 1);
+    n.param<int>("axis_angular", axis_ang, 2);
+    pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+    sub = n.subscribe<sensor_msgs::Joy>("/joy", 10, &Teleop::callback, this);
 }
-void Teleop::callback(const sensor_msgs::Joy::ConstPtr& Joy)  
-{   
-geometry_msgs::Twist v;
-v.linear.x =Joy->axes[axis_lin]*vlinear; //将游戏手柄的数据乘以你想要的速度，然后发给乌龟
- v.angular.z =Joy->axes[axis_ang]*vangular;  
- ROS_INFO("linear:%.3lf angular:%.3lf",v.linear.x,v.angular.z);   
-pub.publish(v);  
-}  
-int main(int argc,char** argv)  
-{  
- ros::init(argc, argv, "logteleop");  
- Teleop telelog;
- ros::spin();
- return 0;  
-}  
+
+void Teleop::callback(const sensor_msgs::Joy::ConstPtr& joy_msg)
+{
+    geometry_msgs::Twist v;
+    v.linear.x = joy_msg->axes[axis_lin] * vlinear;
+    v.angular.z = joy_msg->axes[axis_ang] * vangular;
+    ROS_INFO("linear:%.3lf angular:%.3lf", v.linear.x, v.angular.z);
+    pub.publish(v);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "teleop");
+    ros::NodeHandle n("~");
+    double vel_linear = 0.5; // 默认值
+    double vel_angular = 1.0; // 默认值
+    n.param("vel_linear", vel_linear, 0.5);
+    n.param("vel_angular", vel_angular, 1.0);
+    Teleop teleop(n, vel_linear, vel_angular);
+    ros::spin();
+    return 0;
+}
